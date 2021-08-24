b4b8d34519f4b832a1a6a2af4492e5a0c6e7aa3d
Manuel Castillo <m.castillo@gestionescastillo.com>
2021-08-24 16:02:04 +0200

    Initial routing (Dashboard & Owners List)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OwnersListComponent } from './owners-list/owners-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'owners', component: OwnersListComponent },
+  { path: '**', redirectTo: 'error 404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
